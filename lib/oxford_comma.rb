0daff2fe5251a82ad142becbc357975ebb966584
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-14 11:33:47 +1100

    Done with assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
+
+puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+
+
