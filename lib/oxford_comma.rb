27902165180ee535e0b25b13225654a8b57027bf
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-14 11:32:04 +1100

    commit tree cb49f05a5f514d1b78afffe340b1799fdd7686db

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
+
+puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+
+
+# my_array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+
+# my_array[-2] = "#{my_array[-2]} and"
+
+# puts my_array
